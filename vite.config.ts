fef33271eb37e99bb9625292d93b4dc3dfad3358
Ekaterina Gnedysheva <gnedyshevaeg@gmail.com>
2026-03-07 23:22:59 +0100

    hw2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/homework-front-sprint2/'
+  base: '/homework-front-sprint2/',
+  test: {
+    globals: true,      // позволяет использовать test/expect без импорта
+    environment: 'jsdom' // эмулируем браузер
+  }
 })
